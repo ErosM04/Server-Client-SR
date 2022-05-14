c4cbf98633e509a85ea404cc06ea112f57c8200e
TmsRvl <tommaso.rovoletto@itiszuccante.edu.it>
2022-05-14 18:00:36 +0200

    finish

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -5,48 +5,63 @@ import java.util.*;
 public class Client {
     //per una documentazione + accurata guarda il progetto sul server
     public static void main(String[] args) throws IOException{
-        String nome = "127.0.0.1"; //inserire l'ip sel server
+        String nome = "127.0.0.1";      //inserire l'ip sel server
         int port = 10000;
 
-        Socket client = new Socket(); //oggetto socket, va connesso ad un server
-        client.connect(new InetSocketAddress(nome, port)); //il client attende di connettersi al server, il metodo e' bloccante
-        client.setSoTimeout(30000);
+        Socket client = new Socket();   //oggetto socket, va connesso ad un server
+        client.connect(new InetSocketAddress(nome, port));
+                                        //il client attende di connettersi al server, il metodo è bloccante
 
-        //come sul server va bufferizzata la comunicazione
-        InputStreamReader i = new InputStreamReader(client.getInputStream());
+        client.setSoTimeout(30000);     //il metodo imposta un timeout in millisecondi che verrà avviato ogni colte che
+                                        //viene eseguito un metodo bloccante del socket, se questo timeout scade verrà
+                                        //sollevata un'eccezione.
+
+        //creo un outputWriter per scrivere sul canale di trasmissione
         OutputStreamWriter o = new OutputStreamWriter(client.getOutputStream());
+        //creo un inputReader per leggere dal canale di trasmissione
+        InputStreamReader i = new InputStreamReader(client.getInputStream());
 
-        BufferedReader in = new BufferedReader(i);
+        //il canale va bufferizzato per leggere e scrivere piu' di un carattere alla volta
         BufferedWriter out = new BufferedWriter(o);
+        BufferedReader in = new BufferedReader(i);
+        //così abbiamo bufferizzato i canali
 
-        //Creo il file da cui leggere i dati
         Scanner datiClient = new Scanner(new File("Client/DatiClient.csv"));
-        BufferedWriter datiUtenti = new BufferedWriter(new FileWriter(new File("DatiUtenti")));
+                                        //Scanner che leggerà i dati relativi a un insieme di utenti
 
-        //Scorro il file e man mano che leggo una riga compilo il file finale chiedendo il numero al server
-        while(datiClient.hasNext()){
+        BufferedWriter datiUtenti = new BufferedWriter(new FileWriter(new File("DatiUtenti.csv")));
+                                        //crea o sovrascrive un file che conterrà le informazioni complete degli utenti
+
+        while(datiClient.hasNext()){    //scorro il file contenete i dati degli utenti
             String str[] = datiClient.next().split(";");
-            out.write("give:" + str[0]); // scrivo sul canale di comunicazione la stringa
-            out.newLine();
-            out.flush(); //manda il messaggio al server
+            out.write("give:" + str[0]);
+                                        //chiedo al server il numero di telefono relativo a un determinato utente
+
+            out.newLine();              //invia il messaggio al server
+            out.flush();                //svuota il buffer
             String a = "";
             try{
-                a = in.readLine();
+                a = in.readLine();      //per leggere un input, è bloccante
             }catch (SocketTimeoutException e){
-                break;
+                break;                  //esce dal ciclo while
             }
             datiUtenti.write(str[0] + ";" + str[1] + ";" + str[2] + ";" + checkMessage(a) + "\n");
+                                        //scrivo tutti i dati relativi a un utente nel file finale
         }
 
-
-        //Chiudo la roba
         datiClient.close();
         datiUtenti.close();
         i.close();
         o.close();
-        client.close();
+        client.close();                 //chiudo il socket
     }
 
+    /**
+     * Metodo che estrae il corpo del messaggio passato come parametro
+     *
+     * @param message messaggio del server
+     * @return ritorna il corpo del messaggio inviato, in caso questo sia null ritorna una stringa vuota
+     */
     public static String checkMessage(String message){
         System.out.println(message);
         if(!message.equals(null))
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,7 +3,14 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Server {
-
+    /**
+     * Metodo che data una chiave cerca nel file csv il numero di telefono corrispondente al codice
+     * passato come parametro e lo ritorna
+     *
+     * @param k chiave per la ricerca del numero di telefono
+     * @return ritorna il numero del telefono corrispondente alla chiave
+     * @throws Exception in caso non trovi il file
+     */
     public static String getNumber(String k)throws Exception{
         Scanner sc = new Scanner(new File("Server/DatiServer.csv"));
         while(sc.hasNext()){
@@ -19,35 +26,40 @@ public class Server {
     public static void main(String[] args) throws IOException {
         int port  = 10000;
         ServerSocket server = new ServerSocket(port); //crea server
+
         Socket s = server.accept(); //il server aspetta che un client si connetta
                                     //questo metodo è bloccante
-        s.setSoTimeout(180000);
 
+        s.setSoTimeout(180000);     //il metodo imposta un timeout in millisecondi che verrà avviato ogni colte che
+                                    //viene eseguito un metodo bloccante del socket, se questo timeout scade verrà
+                                    //sollevata un'eccezione.
 
-        OutputStreamWriter o = new OutputStreamWriter(s.getOutputStream()); //creo un outputWriter per scrivere sul canale di trasmissione
-        InputStreamReader i = new InputStreamReader(s.getInputStream()); //creo un inputReader per leggere dal canale di trasmissione
-                                                                            //devo passargli la connessione, il canale
+        //creo un outputWriter per scrivere sul canale di trasmissione
+        OutputStreamWriter o = new OutputStreamWriter(s.getOutputStream());
+        //creo un inputReader per leggere dal canale di trasmissione
+        InputStreamReader i = new InputStreamReader(s.getInputStream());
 
-        //il canale va bufferizzato sennò può leggere e scrivere solo un carattere alla volta :/
+        //il canale va bufferizzato per leggere e scrivere piu' di un carattere alla volta
         BufferedWriter out = new BufferedWriter(o);
         BufferedReader in = new BufferedReader(i);
-                                                    //così abbiamop bufferizzato i canali
+                                                    //così abbiamo bufferizzato i canali
 
-        //--------------------------------Fine implementazione cose importanti------------------------------------------
+        //--------------------------------------------------------------------------------------------------------------
 
-        String a = ""; //stringa da trasmettere
+        String a = "";                          //stringa usata come buffer
 
-        //--------------------------------Fine output-------------------------------------------------------------------
         while(true) {
             try {
-                a = in.readLine(); //per leggere un input, e' bloccante
-            }catch (SocketTimeoutException e){
-                break;
+                a = in.readLine();              //per leggere un input, è bloccante
+            }catch (SocketTimeoutException e){  //quando scade il SO_TIMEOUT
+                break;                          //esce dal ciclo while
             }
             System.out.println(a);
+                                                //se ricevo il comando corretto
             if(a != null && a.substring(0, a.indexOf(":")).equals("give")){
                 try {
                     out.write("find:"+getNumber(a.substring(a.indexOf(":")+1, a.length())));
+                                                //risponde al client con il numero di telefono
                 } catch (Exception e) {
                     System.err.println(e);
                 }
@@ -55,9 +67,10 @@ public class Server {
                 out.flush();
             }
         }
+
         o.close();
         i.close();
-        s.close(); //chiudo la connessione
-        server.close(); //chiude il server
+        s.close();                              //chiudo la connessione
+        server.close();                         //chiude il server
     }
 }
